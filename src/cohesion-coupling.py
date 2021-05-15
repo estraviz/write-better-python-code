92574f1c678afbd039ff58fcde4d2fe8006c1cf7
estraviz <javier.estraviz@gmail.com>
2021-05-16 00:55:06 +0200

    Added part 1 for cohesion and coupling

---
+++ src/cohesion-coupling.py
@@ -0,0 +1,83 @@
+import string
+import random
+
+
+class VehicleInfo:
+    brand: str
+    catalogue_price: int
+    electric: bool
+
+    def __init__(self, brand, electric, catalogue_price):
+        self.brand = brand
+        self.electric = electric
+        self.catalogue_price = catalogue_price
+
+    def compute_tax(self):
+        tax_percentage = 0.05
+        if self.electric:
+            tax_percentage = 0.02
+        return tax_percentage * self.catalogue_price
+
+    def print(self):
+        print(f"Brand: {self.brand}")
+        print(f"Payable tax: {self.compute_tax()}")
+
+
+class Vehicle:
+    id: str
+    license_plate: str
+    info: VehicleInfo
+
+    def __init__(self, id, license_plate, info):
+        self.id = id
+        self.license_plate = license_plate
+        self.info = info
+
+    def print(self):
+        print(f"ID: {self.id}")
+        print(f"License plate: {self.license_plate}")
+        self.info.print()
+
+
+class VehicleRegistry:
+
+    vehicle_info = {}
+
+    def _add_vehicle_info(self, brand, electric, catalogue_price):
+        self.vehicle_info[brand] = VehicleInfo(brand, electric, catalogue_price)
+
+    def __init__(self):
+        self._add_vehicle_info("Tesla Model 3", True, 60_000)
+        self._add_vehicle_info("Volkswagen ID3", True, 35_000)
+        self._add_vehicle_info("BMW 5", False, 45_000)
+        self._add_vehicle_info("Tesla Model Y", True, 75_000)
+
+    def generate_vehicle_id(self, length):
+        return ''.join(random.choices(string.ascii_uppercase, k=length))
+
+    def generate_vehicle_license(self, id):
+        return f"{id[:2]}-{''.join(random.choices(string.digits, k=2))}-{''.join(random.choices(string.ascii_uppercase, k=2))}"
+
+    def create_vehicle(self, brand):
+        vehicle_id = self.generate_vehicle_id(12)
+        license_plate = self.generate_vehicle_license(vehicle_id)
+        return Vehicle(vehicle_id, license_plate, self.vehicle_info[brand])
+
+
+class Application:
+
+    def register_vehicle(self, brand: str):
+        registry = VehicleRegistry()
+        return registry.create_vehicle(brand)
+
+
+if __name__ == '__main__':
+    app = Application()
+
+    print("\nRegistering a BMW 5...")
+    vehicle = app.register_vehicle("BMW 5")
+    vehicle.print()
+
+    print("\nRegistering a Tesla Model Y...")
+    vehicle = app.register_vehicle("Tesla Model Y")
+    vehicle.print()
